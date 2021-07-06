401b9b6161ade4f61d072a96739c53340ccfa449
vlad <lfc2005@mail.ru>
2021-07-06 09:37:26 +0400

    добавление кнопок к заявке для дальнейшего согласования v.4.1

--- template/form_js.php
+++ template/form_js.php
@@ -3,18 +3,18 @@ $local='C:/OpenServer/domains/'.$local_host.'';
 
 if(!isset($no_script)) {
 if ($_SERVER['DOCUMENT_ROOT'] != $local) {
-echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.min.js?cb=1625513248063"></script>';
+echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.min.js?cb=1625549817228"></script>';
 } else {
-echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.js?cb=1625513248063"></script>';
+echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.js?cb=1625549817228"></script>';
 }
 } else
 {
 echo'<script type="text/javascript">';
 
     if ($_SERVER['DOCUMENT_ROOT'] != $local) {
-        echo 'window.src_forms="/public/forms.map.min.js?cb=1625513248063";';
+        echo 'window.src_forms="/public/forms.map.min.js?cb=1625549817228";';
     } else {
-        echo 'window.src_forms="/public/forms.map.js?cb=1625513248063";';
+        echo 'window.src_forms="/public/forms.map.js?cb=1625549817228";';
     }
 
     echo'</script>';

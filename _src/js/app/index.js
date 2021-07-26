37c84bf7f5662dad57875baaeb793e5e838786fc
vlad <lfc2005@mail.ru>
2021-07-26 15:15:04 +0400

    всякое разное

--- _src/js/app/index.js
+++ _src/js/app/index.js
@@ -1473,7 +1473,7 @@ function sl_message_width()
 function maskk(thiss)
 {
 	thiss.val(thiss.val().replace(/[^\d.]*/g, '').replace(/([.])[.]+/g, '$1').replace(/^[^\d]*(\d+([.]\d{0,5})?).*$/g, '$1'));
-	//alert("!!");
+	alert(thiss.val());
 	if($('[name=save_zayy]').length > 0)
 		{
 	savedefault_zay(thiss);
--- template/head.php
+++ template/head.php
@@ -24,12 +24,12 @@
 $local='C:/OpenServer/domains/'.$local_host.'';
 if($_SERVER['DOCUMENT_ROOT']!=$local)
 {
-echo'<link href="/public/main.min.css?cb=1627296674690" type="text/css" rel="stylesheet" />
-<script language="JavaScript" type="text/javascript" src="/public/index.map.min.js?cb=1627296674690"></script>';
+echo'<link href="/public/main.min.css?cb=1627297960769" type="text/css" rel="stylesheet" />
+<script language="JavaScript" type="text/javascript" src="/public/index.map.min.js?cb=1627297960769"></script>';
 } else
 {
-echo'<link href="/_src/css/main.css?cb=1627296674690" type="text/css" rel="stylesheet" />
-<script language="JavaScript" type="text/javascript" src="/public/index.map.js?cb=1627296674690"></script>';
+echo'<link href="/_src/css/main.css?cb=1627297960769" type="text/css" rel="stylesheet" />
+<script language="JavaScript" type="text/javascript" src="/public/index.map.js?cb=1627297960769"></script>';
 }
 
 ?>

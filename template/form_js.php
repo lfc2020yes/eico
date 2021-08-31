644b49009d3131b263fb6d659dd16160c4eabd38
vlad <lfc2005@mail.ru>
2021-08-31 13:10:02 +0400

    график

--- template/form_js.php
+++ template/form_js.php
@@ -3,18 +3,18 @@ $local='C:/OpenServer/domains/'.$local_host.'';
 
 if(!isset($no_script)) {
 if ($_SERVER['DOCUMENT_ROOT'] != $local) {
-echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.min.js?cb=1630390764687"></script>';
+echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.min.js?cb=1630400866754"></script>';
 } else {
-echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.js?cb=1630390764687"></script>';
+echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.js?cb=1630400866754"></script>';
 }
 } else
 {
 echo'<script type="text/javascript">';
 
     if ($_SERVER['DOCUMENT_ROOT'] != $local) {
-        echo 'window.src_forms="/public/forms.map.min.js?cb=1630390764687";';
+        echo 'window.src_forms="/public/forms.map.min.js?cb=1630400866754";';
     } else {
-        echo 'window.src_forms="/public/forms.map.js?cb=1630390764687";';
+        echo 'window.src_forms="/public/forms.map.js?cb=1630400866754";';
     }
 
     echo'</script>';
--- template/head.php
+++ template/head.php
@@ -24,12 +24,12 @@
 $local='C:/OpenServer/domains/'.$local_host.'';
 if($_SERVER['DOCUMENT_ROOT']!=$local)
 {
-echo'<link href="/public/main.min.css?cb=1630390764648" type="text/css" rel="stylesheet" />
-<script language="JavaScript" type="text/javascript" src="/public/index.map.min.js?cb=1630390764648"></script>';
+echo'<link href="/public/main.min.css?cb=1630400866728" type="text/css" rel="stylesheet" />
+<script language="JavaScript" type="text/javascript" src="/public/index.map.min.js?cb=1630400866728"></script>';
 } else
 {
-echo'<link href="/_src/css/main.css?cb=1630390764648" type="text/css" rel="stylesheet" />
-<script language="JavaScript" type="text/javascript" src="/public/index.map.js?cb=1630390764648"></script>';
+echo'<link href="/_src/css/main.css?cb=1630400866728" type="text/css" rel="stylesheet" />
+<script language="JavaScript" type="text/javascript" src="/public/index.map.js?cb=1630400866728"></script>';
 }
 
 ?>

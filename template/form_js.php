07dffcc79b6a0e41a8d3e2c7add11845c7621151
vlad <lfc2005@mail.ru>
2021-09-06 00:57:36 +0400

    новый поставщик в накладных

--- template/form_js.php
+++ template/form_js.php
@@ -3,18 +3,18 @@ $local='C:/OpenServer/domains/'.$local_host.'';
 
 if(!isset($no_script)) {
 if ($_SERVER['DOCUMENT_ROOT'] != $local) {
-echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.min.js?cb=1630400866754"></script>';
+echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.min.js?cb=1630875379840"></script>';
 } else {
-echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.js?cb=1630400866754"></script>';
+echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.js?cb=1630875379840"></script>';
 }
 } else
 {
 echo'<script type="text/javascript">';
 
     if ($_SERVER['DOCUMENT_ROOT'] != $local) {
-        echo 'window.src_forms="/public/forms.map.min.js?cb=1630400866754";';
+        echo 'window.src_forms="/public/forms.map.min.js?cb=1630875379840";';
     } else {
-        echo 'window.src_forms="/public/forms.map.js?cb=1630400866754";';
+        echo 'window.src_forms="/public/forms.map.js?cb=1630875379840";';
     }
 
     echo'</script>';

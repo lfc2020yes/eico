1b04f5904de5e2804d4082b989eae0a1bdef36be
vlad <lfc2005@mail.ru>
2021-09-13 01:43:11 +0400

    перетаскивание файлов для загрузки

--- template/form_js.php
+++ template/form_js.php
@@ -3,18 +3,18 @@ $local='C:/OpenServer/domains/'.$local_host.'';
 
 if(!isset($no_script)) {
 if ($_SERVER['DOCUMENT_ROOT'] != $local) {
-echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.min.js?cb=1631482631042"></script>';
+echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.min.js?cb=1631482967520"></script>';
 } else {
-echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.js?cb=1631482631042"></script>';
+echo '<script language="JavaScript" type="text/javascript" src="/public/forms.map.js?cb=1631482967520"></script>';
 }
 } else
 {
 echo'<script type="text/javascript">';
 
     if ($_SERVER['DOCUMENT_ROOT'] != $local) {
-        echo 'window.src_forms="/public/forms.map.min.js?cb=1631482631042";';
+        echo 'window.src_forms="/public/forms.map.min.js?cb=1631482967520";';
     } else {
-        echo 'window.src_forms="/public/forms.map.js?cb=1631482631042";';
+        echo 'window.src_forms="/public/forms.map.js?cb=1631482967520";';
     }
 
     echo'</script>';

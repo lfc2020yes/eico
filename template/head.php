d3bd2bde38c988a1ef64001cf0396b96e1f953a5
vlad <lfc2005@mail.ru>
2021-09-22 23:08:33 +0400

    наряд max c 3 цифрами после запятой

--- template/head.php
+++ template/head.php
@@ -24,12 +24,12 @@
 $local='C:/OpenServer/domains/'.$local_host.'';
 if($_SERVER['DOCUMENT_ROOT']!=$local)
 {
-echo'<link href="/public/main.min.css?cb=1632336038727" type="text/css" rel="stylesheet" />
-<script language="JavaScript" type="text/javascript" src="/public/index.map.min.js?cb=1632336038727"></script>';
+echo'<link href="/public/main.min.css?cb=1632337606791" type="text/css" rel="stylesheet" />
+<script language="JavaScript" type="text/javascript" src="/public/index.map.min.js?cb=1632337606791"></script>';
 } else
 {
-echo'<link href="/_src/css/main.css?cb=1632336038727" type="text/css" rel="stylesheet" />
-<script language="JavaScript" type="text/javascript" src="/public/index.map.js?cb=1632336038727"></script>';
+echo'<link href="/_src/css/main.css?cb=1632337606791" type="text/css" rel="stylesheet" />
+<script language="JavaScript" type="text/javascript" src="/public/index.map.js?cb=1632337606791"></script>';
 }
 
 ?>

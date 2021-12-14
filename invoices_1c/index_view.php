9f3b793536f6468d5c6cf3285cbd0ecd886337b2
Владислав <65997869+lfc2020yes@users.noreply.github.com>
2021-12-14 11:27:10 +0400

    накладные 1с обработка

--- invoices_1c/index_view.php
+++ invoices_1c/index_view.php
@@ -684,10 +684,11 @@ include_once '../ilib/lib_import.php';
 $csv = new CSV($link, $id_user);
 $mask = $_SERVER['DOCUMENT_ROOT'].'/'.'upload/1c_import/*.csv';
 $mask_attach = $_SERVER['DOCUMENT_ROOT'].'/'.'upload/1c_import/1c_attach/';
-$arFiles = $csv->read_dir ($mask,$mask_attach);
+$arFiles = $csv->read_dir($mask,$mask_attach);
 if(isset($_GET["id"])) {
     //iconv( 'windows-1251','UTF-8',$debug)\
     //echo(base64_decode($_GET['id']));
+    echo(iconv( 'UTF-8','windows-1251',base64_decode($_GET['id'])));
     $data = $csv->read_data(iconv( 'UTF-8','windows-1251',base64_decode($_GET['id'])));
     if(count($data)==0)
     {

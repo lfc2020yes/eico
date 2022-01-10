0ba59c320e8d444bf1f6a5f01e4236a7b3d81285
Владислав <65997869+lfc2020yes@users.noreply.github.com>
2022-01-10 16:49:53 +0400

    накладные 1с проблемы с кодировкой

--- invoices_1c/index_view.php
+++ invoices_1c/index_view.php
@@ -691,7 +691,7 @@ if(isset($_GET["id"])) {
 
 
     //echo(iconv( 'utf-8','windows-1251',rawurldecode($_GET['id'])));
-    $data = $csv->read_data(rawurldecode($_GET['id']));
+    $data = $csv->read_data(base64_decode($_GET['id']));
    // $data = $csv->read_data(my_url_decode(base64_decode($_GET['id'])));
     if(count($data)==0)
     {
